22884474e056be6bb8510daeef0f9f1c1beefe3c
linway035 <wayneck0138@gmail.com>
2022-11-16 21:13:18 +0800

    feat: add express-session & login feature via passport

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
 const methodOverride = require("method-override");
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 3000;
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", "hbs");
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 
 const db = require("./models");
 const Todo = db.Todo;
 const User = db.User;
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require("./config/passport"); //載入一包 Passport 設定檔
+const passport = require("passport"); //把 Passport 套件本身載入進來
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app);
 
 app.get("/", (req, res) => {
   return Todo.findAll({
@@ -37,9 +55,14 @@ app.get("/users/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/users/login", (req, res) => {
-  res.send("login");
-});
+// 加入 middleware，驗證 reqest 登入狀態
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+  })
+);
 
 app.get("/users/register", (req, res) => {
   res.render("register");
