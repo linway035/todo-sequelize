6058460e383742a067ea62846105361da8ea7345
linway035 <wayneck0138@gmail.com>
2022-11-19 12:17:52 +0800

    feat: modify passport.js

--- config/passport.js
+++ config/passport.js
@@ -11,18 +11,22 @@ module.exports = (app) => {
     new LocalStrategy(
       { usernameField: "email", passReqToCallback: true },
       (req, email, password, done) => {
-        User.findOne({ where: { email } })
+        User.findOne({ where: { email } }) //用where
           .then((user) => {
             if (!user) {
-              return done(null, false, {
-                message: "That email is not registered!",
-              });
+              return done(
+                null,
+                false,
+                req.flash("That email is not registered!")
+              );
             }
             return bcrypt.compare(password, user.password).then((isMatch) => {
               if (!isMatch) {
-                return done(null, false, {
-                  message: "Email or Password incorrect.",
-                });
+                return done(
+                  null,
+                  false,
+                  req.flash("Email or Password incorrect.")
+                );
               }
               return done(null, user);
             });
